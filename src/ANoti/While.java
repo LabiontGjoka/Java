fd8c3956c33db163e0419e913fd5ab01b5c22e7a
dardangjok0 <dardangjok0>
2021-04-28 13:56:37 +0200

    dfsdfssdfs

--- src/ANoti/While.java
+++ src/ANoti/While.java
@@ -1,5 +1,5 @@
 package ANoti;
-
+//dfsd
 public class While {
     public static void main(String [] args){
         int i = 0;
--- src/ANoti/baba.java
+++ src/ANoti/baba.java
@@ -1,5 +1,5 @@
 package ANoti;
-
+//fdf
 public class baba {
     public static void main(String[] args) {
         int a =10;
